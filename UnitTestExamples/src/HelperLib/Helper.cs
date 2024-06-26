57f9c81666d163095991227547870218be083816
kamranAeff <kamran_aeff@mail.ru>
2024-06-26 10:28:38 +0400

    initial

---
+++ UnitTestExamples/src/HelperLib/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace HelperLib
+{
+    public static class Helper
+    {
+        public static string Calculate(this DateTime date, DateTime? dateTo = null)
+        {
+            date = date.Date;
+            DateTime now = (dateTo.HasValue ? dateTo.Value.Date : DateTime.Today).AddDays(1);
+
+            if (date > now)
+                throw new ArgumentException(nameof(date));
+
+            int years = now.Year - date.Year,
+                months = now.Month - date.Month,
+                days = now.Day - date.Day;
+
+            if (days < 0)
+            {
+                months--;
+                days += DateTime.DaysInMonth(date.Year, date.Month);
+            }
+
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+
+            var sb = new StringBuilder();
+
+            if (years > 0) sb.AppendFormat("{0}il ", years);
+            if (months > 0) sb.AppendFormat("{0}ay ", months);
+            if (days > 0) sb.AppendFormat("{0}gün ", days);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ UnitTestExamples/tests/HelperLib.Tests/HelperUnitTests.cs
@@ -0,0 +1,42 @@
+namespace HelperLib.Tests
+{
+    [TestFixture]
+    public class HelperUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("2024-06-25", "1gün")]
+        [TestCase("2024-06-01", "25gün")]
+        [TestCase("2024-02-26", "4ay")]
+        [TestCase("2024-02-01", "4ay 25gün")]
+        [TestCase("2021-06-26", "3il")]
+        [TestCase("2021-06-15", "3il 11gün")]
+        [TestCase("2021-02-15", "3il 4ay 11gün")]
+        [TestCase("2024-08-13", "")]
+        public void Test_Experience_Calculation(DateTime date, string expectedResult)
+        {
+            //Arrange
+            DateTime dateTo = new DateTime(2024, 06, 25);
+
+            // Act & Assertion
+            if (date > DateTime.Today)
+            {
+                Assert.Throws<ArgumentException>(() =>
+                {
+                    string actualResult = Helper.Calculate(date, dateTo);
+                });
+            }
+            else
+            {
+                Assert.DoesNotThrow(() =>
+                {
+                    string actualResult = Helper.Calculate(date, dateTo);
+                    Assert.That(actualResult, Is.EqualTo(expectedResult));
+                });
+            }
+        }
+    }
+}
